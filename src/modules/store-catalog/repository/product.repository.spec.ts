e360138242dd1e6d8c10ed3f021d655477918f71
Pedro Furlan <pedrofurlan6@gmail.com>
2025-03-14 00:36:34 -0300

    feat: implementing product repository

--- src/modules/store-catalog/repository/product.repository.spec.ts
+++ src/modules/store-catalog/repository/product.repository.spec.ts
@@ -1,5 +1,6 @@
 import { Sequelize } from "sequelize-typescript";
 import { ProductModel } from "./product.model";
+import ProductRepository from "./product.repository";
 
 describe("Product Repository Test", () => {
   let sequelize: Sequelize;
