730edd3e6958bb4d6b0e019c2b362e68649fabcf
Pedro Furlan <pedrofurlan6@gmail.com>
2025-03-16 21:54:13 -0300

    feat: implementing find method of client repository

---
+++ src/modules/client-adm/repository/client.repository.spec.ts
@@ -0,0 +1,43 @@
+import { Sequelize } from "sequelize-typescript"
+import { ClientModel } from "./client.model";
+import ClientRepository from "./client.repository";
+
+describe("Client Repository Test", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+
+    sequelize.addModels([ClientModel]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it("should find a client", async () => {
+    const client = await ClientModel.create({
+      id: "1",
+      name: "Client 1",
+      email: "email@example.com",
+      address: "Address of client 1",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+
+    const repository = new ClientRepository();
+
+    const result = await repository.find(client.id);
+
+    expect(result.id.id).toBe(client.id);
+    expect(result.name).toBe(client.name);
+    expect(result.email).toBe(client.email);
+    expect(result.address).toBe(client.address);
+  })
+})
