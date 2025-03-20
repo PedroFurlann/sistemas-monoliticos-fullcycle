53dd9145a1effdbf3ae3d824e3d124627584b852
Pedro Furlan <pedrofurlan6@gmail.com>
2025-03-20 15:42:31 -0300

    feat: implementing validateProducts method from PlaceOrder use case

--- src/modules/checkout/usecase/place-order.usecase.ts
+++ src/modules/checkout/usecase/place-order.usecase.ts
@@ -1,18 +1,21 @@
 import UseCaseInterface from "../../@shared/usecase/use-case.interface";
 import ClientAdmFacadeInterface from "../../client-adm/facade/client-adm.facade.interface";
+import ProductAdmFacadeInterface from "../../product-adm/facade/product-adm.facade.interface";
 import { PlaceOrderInputDto, PlaceOrderOutputDto } from "./place-order.dto";
 
 export default class PlaceOrderUseCase implements UseCaseInterface {
   private _clientFacade: ClientAdmFacadeInterface;
+  private _productFacade: ProductAdmFacadeInterface;
   constructor(
-    clientFacade: ClientAdmFacadeInterface
+    clientFacade: ClientAdmFacadeInterface,
+    productFacade: ProductAdmFacadeInterface
   ) {
     this._clientFacade = clientFacade;
+    this._productFacade = productFacade;
   }
 
-
   async execute(input: PlaceOrderInputDto): Promise<PlaceOrderOutputDto> {
-    const client = await this._clientFacade.find({ clientId: input.clientId});
+    const client = await this._clientFacade.find({ clientId: input.clientId });
 
     if (!client) {
       throw new Error("Client not found");
@@ -23,7 +26,6 @@ export default class PlaceOrderUseCase implements UseCaseInterface {
 
     // criar o objeto do client
     // criar o objeto da order(client, products)
-    
 
     // processpayment => facade.processPayment(orderId, amount)
 
@@ -35,6 +37,18 @@ export default class PlaceOrderUseCase implements UseCaseInterface {
   private async validateProducts(input: PlaceOrderInputDto): Promise<void> {
     if (input.products.length === 0) {
       throw new Error("No products selected");
+    }
+
+    for (const product of input.products) {
+      const productAvailable = await this._productFacade.checkStock({
+        productId: product.productId,
+      });
+
+      if (productAvailable.stock <= 0) {
+        throw new Error(
+          `Product ${product.productId} is not available in stock`
+        );
+      }
     }
   }
 }
